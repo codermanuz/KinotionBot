13ea27ba6ed66a4464930d0c26a2956029549626
Muhammad Rasul Nematxonov <abdurasulpython@gmail.com>
2024-08-31 23:33:36 +0500

    update src/handlers/info.py

--- src/handlers/info.py
+++ src/handlers/info.py
@@ -6,4 +6,6 @@ async def get_info(msg: types.Message):
     user_data = db.get_user(msg.from_user.id)
     if user_data:
         user_id, first_name, last_name, banned = user_data
+    else:
+        user_id, first_name, last_name, banned = "aniqlanmagan"
     await msg.answer(f"ID: {user_id}, First Name: {first_name}, Last Name: {last_name}, Banned: {banned}")
