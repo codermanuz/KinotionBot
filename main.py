f21845585d2c93aad0ec7c64b49adc537b1cb441
codermanuz <abdurasulpython@gmail.com>
2024-08-31 22:48:37 +0500

    Fix some bugs

--- main.py
+++ main.py
@@ -13,28 +13,28 @@ bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
 #Initialize startup and shutdown functions
-async def startup_func(bot: Bot, msg: Message):
-  await bot.send_message(
-    text = "Bot run succesfully✅",
-    chat_id = msg.from_user.id,
-  )
+async def startup_func():
+    await bot.send_message(
+      text = "Bot run succesfully✅",
+      chat_id = ADMIN_ID,
+    )
 
-async def shutdown_func(bot: Bot, msg: Message):
-  await bot.send_message(
-    text = "Bot shuted down❗",
-    chat_id = msg.from_user.id,
-  )
+async def shutdown_func():
+    await bot.send_message(
+      text = "Bot shuted down❗",
+      chat_id = ADMIN_ID,
+    )
 
 #Create Main function
-def main():
-  dp.startup.register(startup_func)
-  dp.shutdown.register(shutdown_func)
-  await dp.start_polling(bot)
+async def main():
+    dp.startup.register(startup_func)
+    dp.shutdown.register(shutdown_func)
+    await dp.start_polling(bot)
 
 #Running code
 if __name__ == '__main__':
-  try:
-    asyncio.run(main())
-  except KeyboardInterrupt:
-    print("EXIT!")
+    try:
+      asyncio.run(main())
+    except KeyboardInterrupt:
+      print("EXIT!")
   
