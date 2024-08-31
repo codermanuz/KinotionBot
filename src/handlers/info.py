351afbda09ab6a618fafe23e8d7a6b23ae2bf592
Muhammad Rasul Nematxonov <abdurasulpython@gmail.com>
2024-08-31 23:43:15 +0500

    update src/handlers/info.py

--- src/handlers/info.py
+++ src/handlers/info.py
@@ -2,7 +2,7 @@ from aiogram import types
 from src.db.database import MovieDatabase
 
 async def get_info(msg: types.Message):
-    db = MovieDatabase(dbname="kinobase", user="posegres", password="kinobase")
+    db = MovieDatabase(dbname="kinobase", user="postegres", password="kinobase")
     user_data = db.get_user(msg.from_user.id)
     if user_data:
         user_id, first_name, last_name, banned = user_data
