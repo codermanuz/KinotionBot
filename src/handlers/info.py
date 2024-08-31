825efcce75475dbce877d4c28b51e91a754445ca
Muhammad Rasul Nematxonov <abdurasulpython@gmail.com>
2024-08-31 23:50:11 +0500

    update src/handlers/info.py

--- src/handlers/info.py
+++ src/handlers/info.py
@@ -4,7 +4,7 @@ from src.db.database import db
 async def get_info(msg: types.Message):
     user_data = db.get_user(msg.from_user.id)
     if user_data:
-        user_id, first_name, last_name, banned = user_data[0], user_data[1], user_data[2], user_data[3]
+        user_id, first_name, last_name, banned = user_data[1], user_data[2], user_data[3], user_data[4]
     else:
         user_id, first_name, last_name, banned = "None", "None", "None", "None"
     await msg.answer(f"ID: {user_id}\nFirst Name: {first_name}\nLast Name: {last_name}\nBanned: {banned}")
